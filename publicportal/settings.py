2750bd90e4faa68c92abed6b2884eb2cdc29bd44
Cullen Smith <smith.cmaps@gmail.com>
2024-04-16 08:20:12 -0400

    smallchange

--- publicportal/settings.py
+++ publicportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6fmjtu3+#c0)%r(^#=7d(#$+a*sc)a1c7^o+rlwiajf1fq+a_o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
