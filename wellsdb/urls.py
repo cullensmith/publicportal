8e096640a8ea488eb3ab83857aed1adfb2891435
Cullen Smith <smith.cmaps@gmail.com>
2024-04-18 10:24:29 -0400

    changed to to petro

--- wellsdb/urls.py
+++ wellsdb/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('',views.wellsdb, name="wellsdb"),
     path('cracker',views.cracker, name="cracker"),
     path('terminalspetro',views.terminalspetro, name="terminalspetro"),
-    path('<slug:slug>',views.boxdata, name="boxdata"),
+    path('boxdata',views.boxdata, name="boxdata"),
     path('your_view',views.your_view, name="your_view"),
-
+    path('cracker_states',views.cracker_states, name="cracker_states"),
+    path('added_states',views.added_states, name="added_states"),
+    path('cracker_states2',views.cracker_states2, name="cracker_states2"),
+    path('added_states2',views.added_states2, name="added_states2"),
 ]
--- wellsdb/views.py
+++ wellsdb/views.py
@@ -14,6 +14,57 @@ def cracker(request):
     crackers = Cracker.objects.all()
     return render(request, 'wellsdb.html', { 'crackers': crackers })
 
+def cracker_states(request):
+    print('reached the cracker_states view')
+    states = list()
+    for s in Cracker.objects.all():
+        states.append(s.state)
+    cstates = set(states)
+    print('did we even look?')
+    print(cstates)
+    cstates = list(map(lambda x: x, cstates))
+    return cstates
+
+def added_states(request):
+    # Get the selected states from the AJAX request
+    print('started the added_states view')
+    selected_states = request.GET.getlist('selectedValue[]')
+
+    # Call the cracker_states function to get additional selections
+    additional_selections = cracker_states(selected_states)
+    additional_selections.sort()
+    print(f'new choices: {additional_selections}')
+    # Return the additional selections as JSON response
+    additional_selections = json.dumps(additional_selections)
+
+    return JsonResponse(additional_selections, safe=False)
+
+def cracker_states2(request):
+    print('reached the cracker_states view')
+    states = list()
+    for s in Cracker.objects.all():
+        states.append(s.company)
+    cstates = set(states)
+    print('did we even look?')
+    print(cstates)
+    cstates = list(map(lambda x: x, cstates))
+    return cstates
+
+def added_states2(request):
+    # Get the selected states from the AJAX request
+    print('started the added_states view')
+    selected_states = request.GET.getlist('selectedOption[]')
+
+    # Call the cracker_states function to get additional selections
+    additional_selections = cracker_states2(selected_states)
+    additional_selections.sort()
+    print(f'companies: {additional_selections}')
+    # Return the additional selections as JSON response
+    additional_selections = json.dumps(additional_selections)
+
+    return JsonResponse(additional_selections, safe=False)
+
+
 def terminalspetro(request):
     petroterms = TermPetro.objects.all()
     return render(request, 'wellsdb.html', { 'petroterms': petroterms })
