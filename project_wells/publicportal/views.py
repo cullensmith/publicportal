eb16e4e88fc9791fef07fd2686052dad7631465e
Cullen Smith <smith.cmaps@gmail.com>
2025-03-24 03:21:45 -0400

    moved homepage by changing project views

--- project_wells/publicportal/views.py
+++ project_wells/publicportal/views.py
@@ -1,11 +1,11 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
 
-def homepage(request):
-    #return HttpResponse("Welcome to the Public Facing Data Portal")
-    return render(request, '/wells/templates/wells.html')
-
 # def homepage(request):
 #     #return HttpResponse("Welcome to the Public Facing Data Portal")
-#     return render(request, 'wells.html')
+#     return render(request, '/wells/templates/wells.html')
+
+def homepage(request):
+    #return HttpResponse("Welcome to the Public Facing Data Portal")
+    return render(request, 'wells.html')
 
